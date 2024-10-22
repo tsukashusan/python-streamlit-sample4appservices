858d8e65e495c0b95f18b1dccd2fe83b32dc85f6
tsukabon <shtsukam@outlook.com>
2024-10-23 07:43:55 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+
+# Your Streamlit app code here
+if __name__ == '__main__':
+    st.set_option('server.enableCORS', True)
+    st.write('Hello world.')
---
+++ startup.ps1
@@ -0,0 +1,5 @@
+virtualenv -p python3.12 venv312
+
+.\venv312\Scripts\activate.ps1
+
+pip install -r requirements.txt
